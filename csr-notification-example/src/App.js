2117c8546d4b797f745c6481cb77d93350eff826
Applewhz <76094766+Applewhz@users.noreply.github.com>
2022-02-10 04:23:52 +0800

    commit 1
    notification

--- csr-notification-example/src/App.js
+++ csr-notification-example/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import bell from "./youtubeBell.png"
+import "./App.css";
+import Notifications from "react-notifications-menu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+// const icon = <FontAwesomeIcon icon={faShoppingCart} color="white" size={25} />;
+
+// create component to add new data and to have toast message indicating summary of message
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HELLO WORLD
-        </a>
-      </header>
+      <div className="test">
+        <Notifications
+          data={[
+            {
+              image: logo,
+              message: "Kameshwaran S had shared a feedback with you.",
+              detailPage: "/",
+            },
+            {
+              image: logo,
+              message: (
+                <p>
+                  Kameshwaran S had shared a{" "}
+                  <span style={{ color: "#7ac2fa" }}>feedback</span> with you.
+                </p>
+              ),
+              detailPage: "/",
+            },
+          ]}
+          header={{
+            title: "Notifications",
+            option: { text: "View All", onClick: () => console.log("Clicked") },
+          }}
+        />
+      </div>
     </div>
   );
 }
